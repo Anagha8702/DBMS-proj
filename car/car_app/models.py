74fad3b98d4eba40ddb579b2b5dc2c94bba33c5c
Anagha8702 <anaghaanand.cs20@rvce.edu.in>
2023-02-03 20:23:55 +0530

    schema done

--- car/car_app/models.py
+++ car/car_app/models.py
@@ -19,15 +19,36 @@ class Customer_Review(models.Model):
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['Customer_ID', 'Customer_Review'], name='unique_host_migration'),
+            models.UniqueConstraint(fields=['Customer_ID', 'Customer_Review'], name='unique_host_migration1'),
         ]
+
+class Engine(models.Model):
+    Engine_ID= models.IntegerField(primary_key= True)
+    cc = models.IntegerField()
+    fuel_system_type = models.CharField(max_length=20)
+    capacity = models.IntegerField()
+    fuel_type = models.CharField(max_length=20)
 
 class Car(models.Model):
     Car_ID= models.IntegerField(primary_key= True)
+    variant=models.CharField(max_length=10)
+    Model=models.CharField(max_length=20)
+    Mileage = models.FloatField()
+    Make = models.CharField(max_length=20)
+    kerb_weight = models.IntegerField()
+    Type=models.CharField(max_length=10)
+    Admin_ID = models.ForeignKey(Admin, on_delete=models.SET_NULL, null=True)
+    Engine_ID = models.ForeignKey(Engine, on_delete=models.SET_NULL, null=True)
+  
+class Cylinder(models.Model):
+    no_of_cylinders = models.IntegerField()
+    valves_per_cylinder = models.IntegerField()
+    config = models.CharField(max_length=20)
+    class Meta:
+            constraints = [
+                models.UniqueConstraint(fields=['valves_per_cylinder', 'config'], name='unique_host_migration2'),
+            ]
 
-
-    
-
-
-
-
+class Bought_by(models.Model):
+    Customer_ID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    Car_ID = models.ForeignKey(Car, on_delete=models.CASCADE)

1f5cebe187a3768ddf9f382f3e41cc24ffee5106
Anagha8702 <anaghaanand.cs20@rvce.edu.in>
2023-02-03 19:20:43 +0530

    new commit

---
+++ car/car_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Admin(models.Model):
+    Admin_ID = models.IntegerField(primary_key= True)
+    Admin_Name= models.CharField(max_length=25)
+    address= models.CharField(max_length=100)
+    salary= models.FloatField()
+    Phone_no= models.CharField(max_length=14)
+
+class Customer(models.Model):
+    Customer_ID = models.IntegerField(primary_key= True)
+    Customer_Name= models.CharField(max_length=25)
+    License_No = models.IntegerField()
+
+class Customer_Review(models.Model):
+    Customer_Review= models.CharField(max_length=100)
+    Customer_ID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['Customer_ID', 'Customer_Review'], name='unique_host_migration'),
+        ]
+
+
+    
+
+
+
+
